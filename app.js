f7271246b49983e16413648d4db1df32609d417d
Alexander J Massimino <49351994+alexmassimino9@users.noreply.github.com>
2022-03-04 03:56:59 -0800

    added procfile

--- app.js
+++ app.js
@@ -59,7 +59,7 @@ app.post("/failure", function (req, res) {
   res.redirect("/")
 })
  
-app.listen(3000, function (req, res) {
+app.listen( process.env.PORT || 3000, function (req, res) {
   console.log("Server is running on port 3000");
 })
 
